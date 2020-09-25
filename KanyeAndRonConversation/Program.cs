1105fd4483cadd2fef85b6fade0088a1a7b157e0
aburbank <ashlynburbank@gmail.com>
2020-09-24 20:43:29 -0500

    finished beginning stage

---
+++ KanyeAndRonConversation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeAndRonConversation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hey Kanye and Ron!");
+            Console.WriteLine($"Tell me some of your thoughts?");
+
+
+            for (int i = 0; i < 1; i++)
+            {
+                QuoteGenerator.KanyeQuote();
+
+                QuoteGenerator.RonQuote();
+            }
+            
+        }
+    }
+}
---
+++ KanyeAndRonConversation/QuoteGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeAndRonConversation
+{
+    public class QuoteGenerator
+    {
+        //Method 1 (Kanye Quotes)
+
+        public static void KanyeQuote()
+        {
+
+            var kanyeURL = "https://api.kanye.rest";
+
+            var client = new HttpClient();
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+
+            Console.WriteLine($"");
+            Console.WriteLine($"Kanye: '{kanyeQuote}'");
+            Console.WriteLine($"");
+
+        }
+
+
+        //Method 2 (Ron Quotes)
+
+        public static void RonQuote()
+        {
+            var client = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('{', ' ').Replace('}', ' ').Trim();
+
+
+
+            Console.WriteLine($"");
+            Console.WriteLine($"Ron: '{ronQuote}'");
+            Console.WriteLine($"");
+
+
+        }
+
+
+    }
+}
